af6ad6362611c32f9d23b77047e4ba5f97b1dffe
Juan Rivera <juan.rivera@funnel.io>
2023-05-17 19:55:47 -0400

    intro: adding three point statistics to player class

--- intro/player.py
+++ intro/player.py
@@ -1,9 +1,14 @@
 class Player:
-    def __init__(self, name, pos, fgm, fga):
+    def __init__(self, name, pos, fgm, fga, threes_m, threes_a):
         self.name = name
         self.pos = pos
         self.fgm = fgm
         self.fga = fga
+        self.threes_m = threes_m
+        self.threes_a = threes_a
 
     def fg_percent(self):
         return self.fgm / self.fga
+
+    def threes_percent(self):
+        return self.threes_m / self.threes_a
